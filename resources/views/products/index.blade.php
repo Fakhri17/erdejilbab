5e404e267a51d3b414625a244663a2dadf1a45a6
Fakhri17 <fakhricoffe@gmail.com>
2025-05-06 15:37:47 +0700

    fix route

---
+++ resources/views/products/index.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('title', 'List Produk')
+
+@section('content')
+  <section>
+    <div class="container mx-auto px-4 py-8">
+      <h1 class="text-3xl font-bold mb-4 text-secondary">Welcome to the Product List</h1>
+      <p class="text-gray-700">This is a simple Laravel application using Tailwind CSS.</p>
+    </div>
+  </section>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
+
+Route::get('list-produk', function () {
+    return view('products.index');
+});
