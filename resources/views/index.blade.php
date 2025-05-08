d03b0f942a951ef4b698697f21a25d96aadac5a4
Fakhri17 <fakhricoffe@gmail.com>
2025-05-08 15:44:23 +0700

    fix: desain like figma

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,169 +4,169 @@
 
 @section('content')
   <!-- Hero Section -->
-  <section class="py-10 bg-white">
-    <div class="max-w-6xl mx-auto px-4 flex flex-col-reverse md:flex-row justify-between items-center gap-8">
-      <div class="md:w-1/2 text-center md:text-left">
-        <h1 class="text-3xl md:text-4xl font-bold mb-5 leading-normal">
-          Tampil Anggun & Syar'i <br />Bersama <span class="text-[#E48786]">Erdejilbab</span>
-        </h1>
-        <p class="text-gray-600 mb-8 pr-8">
-          Pilihan sempurna untuk gaya muslimah modern, eksklusif hanya di Erdejilbab.
-        </p>
-        <a href="#"
-          class="inline-block px-6 py-3 font-bold text-[#946756] rounded border border-gray-300 shadow hover:bg-[#E48786] hover:text-white transition duration-200">
-          Lihat Produk
-        </a>
-      </div>
-      <div class="md:w-1/2 flex justify-center">
-        <img src="/assets/heroimg.png" alt="Model Hijab" class="max-w-xs w-full md:max-w-md rounded-xl">
+  <!-- Hero Section -->
+  <section class="py-24">
+    <div class="container mx-auto px-4 py-8">
+      <div class="max-w-6xl mx-auto flex flex-col-reverse md:flex-row justify-between items-center gap-8">
+        <div class="md:w-1/2 text-center md:text-left">
+          <h1 class="text-3xl md:text-4xl font-bold mb-5 leading-normal">
+            Tampil Anggun & Syar'i <br />Bersama <span class="text-[#E48786]">Erdejilbab</span>
+          </h1>
+          <p class="text-gray-800 font-semibold mb-8 pr-8">
+            Pilihan sempurna untuk gaya muslimah modern, eksklusif hanya di Erdejilbab.
+          </p>
+          <a href="#"
+            class="inline-block px-5 py-2 font-bold text-[#946756] rounded-md border border-gray-800 shadow-md hover:border-[#E48786] hover:bg-[#E48786] hover:text-white transition duration-200">
+            Lihat Produk
+          </a>
+        </div>
+        <div class="md:w-1/2 flex justify-center">
+          <img src="{{ asset('assets/heroimg.png') }}" alt="Model Hijab" class="max-w-xs w-full md:max-w-md rounded-xl">
+        </div>
       </div>
     </div>
   </section>
 
   <!-- Keunggulan Section -->
-  <section class="max-w-6xl mx-auto py-10 text-center">
-    <h2 class="text-2xl lg:text-3xl font-bold mb-4">Keunggulan <span class="text-[#E48786]">Erdejilbab</span></h2>
-    <p class="text-gray-600 max-w-2xl mx-auto mb-10">
-      Mengapa Erdejilbab Menjadi Pilihan Tepat untuk Tampil Syar'i dan Elegan
-    </p>
+  <section class="py-24">
+    <div class="container mx-auto px-4 py-8 text-center">
+      <h2 class="text-2xl lg:text-3xl font-bold mb-4">Keunggulan <span class="text-[#E48786]">Erdejilbab</span></h2>
+      <p class="text-gray-600 max-w-2xl mx-auto mb-10">
+        Mengapa Erdejilbab Menjadi Pilihan Tepat untuk Tampil Syar'i dan Elegan
+      </p>
 
-    <div class="grid grid-cols-1 md:grid-cols-3 gap-[5vw]">
-      <!-- Card 1 -->
-      <div class="bg-white rounded-xl p-6">
-        <div class="flex flex-col md:flex-row items-center md:items-start gap-4 mb-3 text-center md:text-left">
-          <div class="flex flex-col md:flex-row items-center md:items-center gap-4">
-            <img src="/assets/icon1.png" alt="Bahan Berkualitas" class="w-12 h-12">
-            <h1 class="font-bold text-lg">Bahan Berkualitas</h1>
+      <div class="grid grid-cols-1 md:grid-cols-3 gap-[5vw]">
+        <!-- Card 1 -->
+        <div class="bg-white rounded-xl p-6">
+          <div class="flex flex-col md:flex-row items-center md:items-start gap-4 mb-3 text-center md:text-left">
+            <div class="flex flex-col md:flex-row items-center md:items-center gap-4">
+              <iconify-icon icon="hugeicons:hijab" width="32" height="32"
+                class="bg-primary p-2 text-white rounded-xl"></iconify-icon>
+              <h1 class="font-bold text-xl">Bahan Berkualitas</h1>
+            </div>
           </div>
+          <p class="text-gray-600 text-base font-medium text-center md:text-left">
+            Menggunakan bahan pilihan yang lembut, ringan, dan tidak panas, sehingga nyaman dipakai seharian.
+          </p>
         </div>
-        <p class="text-gray-600 text-sm text-center md:text-left">
-          Kualitas premium dengan harga yang bersahabat. Karena tampil anggun tak harus mahal.
-        </p>
-      </div>
-      <!-- Card 2 -->
-      <div class="bg-white rounded-xl p-6">
-        <div class="flex flex-col md:flex-row items-center md:items-start gap-4 mb-3 text-center md:text-left">
-          <div class="flex flex-col md:flex-row items-center md:items-center gap-4">
-            <img src="/assets/icon2.png" alt="Desain Eksklusif" class="w-12 h-12">
-            <h1 class="font-bold text-lg">Desain Eksklusif</h1>
+        <!-- Card 2 -->
+        <div class="bg-white rounded-xl p-6">
+          <div class="flex flex-col md:flex-row items-center md:items-start gap-4 mb-3 text-center md:text-left">
+            <div class="flex flex-col md:flex-row items-center md:items-center gap-4">
+              <iconify-icon icon="fluent:design-ideas-48-regular" width="32" height="32"
+                class="bg-primary p-2 text-white rounded-xl"></iconify-icon>
+              <h1 class="font-bold text-xl">Desain Eksklusif</h1>
+            </div>
           </div>
+          <p class="text-gray-600 text-base font-medium text-center md:text-left">
+            Kualitas premium dengan harga yang bersahabat. Karena tampil anggun tak harus mahal.
+          </p>
         </div>
-        <p class="text-gray-600 text-sm text-center md:text-left">
-          Kualitas premium dengan harga yang bersahabat. Karena tampil anggun tak harus mahal.
-        </p>
-      </div>
-      <!-- Card 3 -->
-      <div class="bg-white rounded-xl p-6">
-        <div class="flex flex-col md:flex-row items-center md:items-start gap-4 mb-3 text-center md:text-left">
-          <div class="flex flex-col md:flex-row items-center md:items-center gap-4">
-            <img src="/assets/icon3.png" alt="Harga Terjangkau" class="w-12 h-12">
-            <h1 class="font-bold text-lg">Harga Terjangkau</h1>
+        <!-- Card 3 -->
+        <div class="bg-white rounded-xl p-6">
+          <div class="flex flex-col md:flex-row items-center md:items-start gap-4 mb-3 text-center md:text-left">
+            <div class="flex flex-col md:flex-row items-center md:items-center gap-4">
+              <iconify-icon icon="solar:tag-price-outline" width="32" height="32"
+                class="bg-primary p-2 text-white rounded-xl"></iconify-icon>
+              <h1 class="font-bold text-xl">Harga Terjangkau</h1>
+            </div>
           </div>
+          <p class="text-gray-600 text-base font-medium text-center md:text-left">
+            Kualitas premium dengan harga yang bersahabat. Karena tampil anggun tak harus mahal.
+          </p>
         </div>
-        <p class="text-gray-600 text-sm text-center md:text-left">
-          Kualitas premium dengan harga yang bersahabat. Karena tampil anggun tak harus mahal.
-        </p>
       </div>
     </div>
   </section>
 
   <!-- Section Produk Unggulan -->
-  <section class="max-w-6xl mx-auto px-4 py-10 relative">
-      <div class="flex items-center justify-between mb-4">
+  <section class="py-24 relative">
+    <div class="container mx-auto px-4 py-8">
+      <div class="lg:flex items-center justify-between mb-4">
         <h2 class="text-2xl lg:text-3xl font-bold">Produk <span class="text-[#E48786]">Unggulan</span></h2>
       </div>
-      <div class="flex items-center justify-between mb-6 pb-4">
-        <p class="text-sm text-gray-500">Pilihan Terbaik untuk Tampil Cantik dan Syar'i Setiap Hari</p>
-        <a href="#" class="font-bold text-[#946756] hover:text-[#E48786]">Lihat Semua &gt;</a>
+      <div class="block lg:flex items-center justify-between mb-6 pb-4">
+        <p class="text-base text-gray-700 font-medium mb-3 lg:mb-0">Pilihan Terbaik untuk Tampil Cantik dan Syar'i Setiap
+          Hari</p>
+        <a href="#" class="font-bold text-[#946756] hover:text-[#E48786] flex items-center gap-2">Lihat Semua
+          <iconify-icon icon="ep:arrow-right-bold"></iconify-icon></a>
       </div>
 
-      <div class="absolute top-[60%] left-1/2 -translate-x-1/2 -translate-y-1/2 w-96 h-96 bg-[#946756] rounded-full opacity-50 blur-3xl"></div>
+      <div
+        class="absolute top-[60%] left-1/2 -translate-x-1/2 -translate-y-1/2 w-96 h-96 bg-[#946756] rounded-full opacity-50 blur-3xl">
+      </div>
       <div class="relative z-10">
-  
-      <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
-        <!-- Produk Card -->
-        <div class="bg-white border border-gray-300 rounded-xl p-4">
-          <img src="/assets/prodimg.png" alt="Pashmina Crinkle Erdejilbab" class="rounded-lg mb-4 w-full h-60 object-cover">
-          <div class="flex flex-col items-center">
-            <h3 class="font-semibold text-lg text-[#946756] mb-1">Pashmina Crinkle Erdejilbab</h3>
-            <p class="text-sm text-gray-500 mb-4 text-center mx-4">Bahan katun ombre super dengan ukuran 180x80cm</p>
-            <button
-              class="text-sm text-[#946756] hover:text-white hover:bg-[#E48786] border border-gray-300 px-4 py-2 rounded transition duration-200">Lihat
-              Produk</button>
-          </div>
-        </div>
-
-        <div class="bg-white border border-gray-300 rounded-xl p-4">
-          <div class="flex flex-col items-center">
-            <img src="/assets/prodimg.png" alt="Pashmina Kaos Rayon Super" class="rounded-lg mb-4 w-full h-60 object-cover">
-            <h3 class="font-semibold text-lg text-[#946756] mb-1">Pashmina Kaos Rayon Super</h3>
-            <p class="text-sm text-gray-500 mb-4 text-center mx-4">Bahan rayon spandex super dengan ukuran 180x60cm</p>
-          <button
-              class="text-sm text-[#946756] hover:text-white hover:bg-[#E48786] border border-gray-300 px-4 py-2 rounded transition duration-200">Lihat
-              Produk</button>
-          </div>
-        </div>
-
-        <div class="bg-white border border-gray-300 rounded-xl p-4">
-          <div class="flex flex-col items-center">
-            <img src="/assets/prodimg.png" alt="Hijab Azara Voal Segi Empat" class="rounded-lg mb-4 w-full h-60 object-cover">
-            <h3 class="font-semibold text-lg text-[#946756] mb-1">Hijab Azara Voal Segi Empat</h3>
-            <p class="text-sm text-gray-500 mb-4 text-center mx-4">Bahan voal miracle ori Azara dengan ukuran 110x110cm</p>
-          <button
-            class="text-sm text-[#946756] hover:text-white hover:bg-[#E48786] border border-gray-300 px-4 py-2 rounded transition duration-200">Lihat
-            Produk</button>
+        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+          <!-- Produk Card -->
+          @for ($i = 0; $i < 3; $i++)
+            <div class="bg-white border border-gray-300 rounded-xl p-4">
+              <img src="{{ asset('assets/prodimg.png') }}" alt="Pashmina Crinkle Erdejilbab"
+                class="rounded-lg mb-4 w-full h-60 object-cover">
+              <div class="flex flex-col items-center">
+                <h3 class="font-bold text-xl text-[#946756]">Pashmina Crinkle Erdejilbab</h3>
+                <p class="text-sm text-gray-700 font-medium my-4 text-center mx-4">Model Pashmina yang sedang hits....</p>
+                <a href="#"
+                  class="inline-block text-sm px-5 py-2 font-semibold text-[#946756] rounded-md border border-gray-500 hover:border-[#E48786] hover:bg-[#E48786] hover:text-white transition duration-200">Lihat
+                  Produk</a>
+              </div>
+            </div>
+          @endfor
         </div>
       </div>
     </div>
   </section>
 
   <!-- Section Hubungi Kami -->
-  <section class="max-w-6xl mx-auto px-4 py-10 text-center">
-    <h2 class="text-2xl font-bold lg:text-3xl mb-4">Hubungi <span class="text-[#E48786]">Kami</span></h2>
-    <p class="text-sm text-gray-600 mt-2 mb-8">Ada pertanyaan, saran, atau ingin memesan langsung?</p>
+  <section class="py-24">
+    <div class="container mx-auto px-4 py-8 text-center">
+      <div class="mb-10">
+        <h2 class="text-2xl font-bold lg:text-3xl mb-4">Hubungi <span class="text-[#E48786]">Kami</span></h2>
+        <p class="text-base text-gray-700 font-medium">Ada pertanyaan, saran, atau ingin memesan langsung?</p>
+      </div>
 
-    <div class="grid grid-cols-1 sm:grid-cols-3 gap-6 text-center">
-      <!-- Shopee -->
-      <div class="border border-gray-300 hover:border-3 hover:border-[#E48786] rounded-xl p-10 bg-white transition duration-300">
-        <div class="flex flex-col items-center md:items-start text-center md:text-left">
-          <img src="/assets/shope.png" alt="Shopee Icon" class="mb-3" >
-          <h1 class="font-bold text-lg mb-1">Shopee</h1>
-          <p class="text-sm text-gray-500 mb-4">
-            Dapatkan produk Erdejilbab di Shopee dengan promo menarik!
-          </p>
-          <a href="#"
-            class="text-sm text-[#946756] hover:text-white hover:bg-[#E48786] border border-gray-300 px-4 py-2 rounded transition duration-200">
-            Kunjungi
-          </a>
+      <div class="grid grid-cols-1 sm:grid-cols-3 gap-6 text-center">
+        <!-- Shopee -->
+        <div
+          class="border-2 border-gray-300 hover:border-[#E48786] rounded-xl p-10 bg-white transition duration-300">
+          <div class="flex flex-col items-center md:items-start text-center md:text-left">
+            <iconify-icon icon="tabler:brand-shopee" width="48" height="48" class="mb-3"></iconify-icon>
+            <h1 class="font-bold text-xl mb-2">Shopee</h1>
+            <p class="text-sm text-gray-700 font-medium mb-4">
+              Dapatkan produk Erdejilbab di Shopee dengan promo menarik!
+            </p>
+            <a href="#"
+              class="inline-block text-sm px-5 py-2 font-semibold text-[#946756] rounded-md border border-gray-500 
+              hover:border-[#E48786] hover:bg-[#E48786] hover:text-white transition duration-200">
+              Kunjungi</a>
+          </div>
         </div>
-      </div>
-      <!-- WhatsApp -->
-      <div class="border border-gray-300 hover:border-3 hover:border-[#E48786] rounded-xl p-10 bg-white transition duration-300">
-        <div class="flex flex-col items-center md:items-start text-center md:text-left">
-          <img src="/assets/wa.png" alt="WhatsApp Icon" class="mb-3">
-          <h1 class="font-bold text-lg mb-1">WhatsApp</h1>
-          <p class="text-sm text-gray-500 mb-4">
-            Hubungi kami langsung untuk konsultasi & pemesanan.
-          </p>
-          <a href="#"
-            class="text-sm text-[#946756] hover:text-white hover:bg-[#E48786] border border-gray-300 px-4 py-2 rounded transition duration-200">
-            Kunjungi
-          </a>
+        <!-- WhatsApp -->
+        <div
+          class="border-2 border-gray-300 hover:border-[#E48786] rounded-xl p-10 bg-white transition duration-300">
+          <div class="flex flex-col items-center md:items-start text-center md:text-left">
+            <iconify-icon icon="ic:baseline-whatsapp" width="48" height="48" class="mb-3"></iconify-icon>
+            <h1 class="font-bold text-xl mb-2">WhatsApp</h1>
+            <p class="text-sm text-gray-700 font-medium mb-4">
+              Hubungi kami langsung untuk konsultasi & pemesanan.
+            </p>
+            <a href="#"
+              class="inline-block text-sm px-5 py-2 font-semibold text-[#946756] rounded-md border-2 border-gray-300 
+              hover:border-[#E48786] hover:bg-[#E48786] hover:text-white transition duration-200">Hubungi</a>
+          </div>
         </div>
-      </div>
-      <!-- TikTok -->
-      <div class="border border-gray-300 hover:border-3 hover:border-[#E48786] rounded-xl p-10 bg-white transition duration-300">
-        <div class="flex flex-col items-center md:items-start text-center md:text-left">
-          <img src="/assets/tiktok.png" alt="Tiktok Icon" class="mb-3">
-          <h1 class="font-bold text-lg mb-1">TikTok</h1>
-          <p class="text-sm text-gray-500 mb-4">
-            Temukan koleksi dari video kami dan checkout tanpa ribet!
-          </p>
-          <a href="#"
-            class="text-sm text-[#946756] hover:text-white hover:bg-[#E48786] border border-gray-300 px-4 py-2 rounded transition duration-200">
-            Kunjungi
-          </a>
+        <!-- TikTok -->
+        <div
+          class="border-2 border-gray-300 hover:border-[#E48786] rounded-xl p-10 bg-white transition duration-300">
+          <div class="flex flex-col items-center md:items-start text-center md:text-left">
+            <iconify-icon icon="mingcute:tiktok-line" width="48" height="48" class="mb-3"></iconify-icon>
+            <h1 class="font-bold text-xl mb-2">TikTok</h1>
+            <p class="text-sm text-gray-700 font-medium mb-4">
+              Temukan koleksi dari video kami dan checkout tanpa ribet!
+            </p>
+            <a href="#"
+              class="inline-block text-sm px-5 py-2 font-semibold text-[#946756] rounded-md border-2 border-gray-300 hover:border-[#E48786] 
+              hover:bg-[#E48786] hover:text-white transition duration-200">Kunjungi</a>
+          </div>
         </div>
       </div>
     </div>
--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -51,20 +51,20 @@
 @endphp --}}
 
 @section('content')
-  <section>
+  <section class="pt-24 pb-12">
     <div class="container mx-auto px-4 py-8">
       <div class="flex flex-col gap-4 md:flex-row md:items-center md:justify-between">
         <div class="flex flex-row flex-wrap items-center gap-2">
           <!-- Button Terbaru -->
-          <button
+          {{-- <button
             class="bg-white font-light text-sm py-2 px-4 border border-gray-300 rounded-md hover:bg-gray-100 shadow-md cursor-pointer">
             Terbaru
-          </button>
+          </button> --}}
 
           <!-- Button Dropdown Kategori -->
           <div class="hs-dropdown relative inline-flex">
             <button id="hs-dropdown-example" type="button"
-              class="hs-dropdown-toggle py-2 px-4 inline-flex items-center gap-x-2 text-sm font-light border border-gray-300 rounded-md bg-white hover:bg-gray-100 shadow-md cursor-pointer"
+              class="hs-dropdown-toggle py-2.5 px-5 inline-flex items-center gap-x-2 text-base font-medium border border-gray-300 rounded-md bg-white hover:bg-gray-100 shadow-md cursor-pointer"
               aria-haspopup="menu" aria-expanded="false">
               Kategori
               <svg class="hs-dropdown-open:rotate-180 size-4 text-gray-600" xmlns="http://www.w3.org/2000/svg"
@@ -88,12 +88,8 @@
           <input type="text" placeholder="Cari Produk"
             class="w-full px-4 py-2 text-sm font-light border border-gray-300 rounded-l-md focus:outline-none shadow-md" />
           <button
-            class="bg-[#8B5E3C] hover:bg-[#7a4f31] text-white px-4 py-2 border border-gray-300 border-l-0 rounded-r-md shadow-md cursor-pointer">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24"
-              stroke="currentColor" stroke-width="2">
-              <path stroke-linecap="round" stroke-linejoin="round"
-                d="M21 21l-4.35-4.35m0 0A7.5 7.5 0 1010.5 18a7.5 7.5 0 006.15-3.35z" />
-            </svg>
+            class="bg-[#8B5E3C] hover:bg-[#7a4f31] text-white px-4 py-2 border border-gray-300 border-l-0 rounded-r-md shadow-md cursor-pointer flex items-center justify-center">
+            <iconify-icon icon="iconamoon:search" width="20" height="20"></iconify-icon>
           </button>
         </div>
       </div>
@@ -101,18 +97,50 @@
   </section>
 
 
-  <section>
+  <section class="">
     <div class="container mx-auto px-4 py-8">
       <!-- Judul -->
-      <h1 class="text-2xl sm:text-3xl md:text-4xl font-bold">
-        Produk <span class="text-[#e48786]">Erdejilbab</span>
-        <span class="opacity-50 font-normal text-2xl sm:text-3xl md:text-4xl"></span>
-      </h1>
+      <div class="flex items-center gap-2">
+        <h1 class="text-2xl sm:text-3xl md:text-4xl font-bold">
+          Produk <span class="text-[#e48786]">Erdejilbab</span>
+        </h1>
+        <span class="text-lg sm:text-xl md:text-2xl text-gray-500">(14)</span>
+      </div>
 
       <!-- Card Produk -->
-      <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-8">
+      <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 mt-8">
         @for ($i = 0; $i < 9; $i++)
           <div
+            class="flex flex-col group bg-white border border-gray-200 shadow-2xs rounded-xl 
+            overflow-hidden hover:shadow-lg focus:outline-hidden focus:shadow-lg transition">
+            <div class="relative pt-[50%] sm:pt-[60%] lg:pt-[80%] rounded-t-xl overflow-hidden">
+              <img
+                class="size-full absolute top-0 start-0 object-cover group-hover:scale-105 group-focus:scale-105 transition-transform duration-500 ease-in-out rounded-t-xl"
+                src="{{ asset('assets/image-14.png') }}" alt="Card Image">
+            </div>
+            <div class="p-4 md:p-5">
+
+              <h3 class="text-xl font-bold text-secondary">
+                Card title
+              </h3>
+              <div class="mt-1">
+                <p class="text-sm text-gray-700 font-medium mt-4 mb-6">
+                  Some quick example text to build on the card title and make up the bulk of the card's content.
+                </p>
+                <div class="mt-3 flex items-center justify-between">
+                  <p class="text-base font-bold text-black">
+                    Rp200.000
+                  </p>
+                  <a href="#"
+                    class="inline-block px-3 py-1.5 text-sm font-bold text-[#946756] rounded-md border border-gray-400 hover:border-[#E48786] hover:bg-[#E48786] hover:text-white transition duration-200">
+                    Lihat Produk
+                  </a>
+                </div>
+              </div>
+
+            </div>
+          </div>
+          {{-- <div
             class="flex flex-col bg-white border-none rounded-xl overflow-hidden hover:shadow-lg transition-shadow duration-300">
             <!-- Gambar -->
             <div class="aspect-[4/5] overflow-hidden">
@@ -120,26 +148,27 @@
                 class="w-full h-full rounded-xl object-cover transition-transform duration-500 ease-in-out hover:scale-105">
             </div>
             <!-- Konten -->
-            <div class="p-4 md:p-5 flex flex-col justify-between flex-grow">
+            <div class="p-3 flex flex-col justify-between flex-grow">
               <div>
-                <h3 class="text-base sm:text-lg md:text-lg font-bold">
+                <h3 class="text-base font-bold text-primary">
                   Produk Hijab
                 </h3>
-                <p class="mt-1 text-sm sm:text-base text-gray-500 dark:text-neutral-400">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium inventore soluta omnis consequatur unde ipsum deserunt reprehenderit tempore, esse aut nisi beatae, sint magni vero sit! Iusto ea incidunt dolor!
+                <p class="mt-1 text-sm text-gray-500 dark:text-neutral-400 line-clamp-2">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium inventore soluta omnis consequatur
+                  unde ipsum deserunt reprehenderit tempore.
                 </p>
               </div>
-              <div class="mt-4 flex items-center justify-between">
-                <p class="text-base sm:text-lg md:text-xl font-bold text-black">
+              <div class="mt-3 flex items-center justify-between">
+                <p class="text-base font-bold text-black">
                   Rp200.000
                 </p>
                 <a href="#"
-                  class="text-sm sm:text-lg text-[#8B5E3C] border border-[#8B5E3C] font-semibold rounded-lg px-4 py-1 hover:bg-[#8B5E3C] hover:text-white transition">
-                  Lihat Produk
+                  class="inline-block px-3 py-1.5 text-sm font-bold text-[#946756] rounded-md border border-gray-400 hover:border-[#E48786] hover:bg-[#E48786] hover:text-white transition duration-200">
+                  Lihat
                 </a>
               </div>
             </div>
-          </div>
+          </div> --}}
         @endfor
 
       </div>
