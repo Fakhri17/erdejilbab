32c36dca4cbca671e39de9d2a68619751360eb61
Fakhri17 <fakhricoffe@gmail.com>
2025-05-13 23:03:39 +0700

    fix migrations

--- database/migrations/2025_05_13_155033_add_is_best_seller_to_products_table.php
+++ database/migrations/2025_05_13_155033_add_is_best_seller_to_products_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('products', function (Blueprint $table) {
-            $table->boolean('is_best_seller')->default(false);
+            $table->boolean('is_best_seller')->default(false)->after('color');
         });
     }
 
